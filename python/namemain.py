736fa933d8c6347517ae2511fef7effc8094b6c4
Sadashiv <sadamsrit@gmail.com>
2013-09-17 20:25:11 +0530

    Added the namemain and module

---
+++ python/namemain.py
@@ -0,0 +1,11 @@
+print "srinivas"
+print "sadashiv"
+
+print __name__
+
+if __name__ == "__main__":
+    print __name__
+    print "Srinivas"
+    print "Sadashiv"
+#__name__ will contain __main__
+#while import it will take program or module
