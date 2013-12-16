dbd6d9bfd58080e9dd10aaaafb6df07e3b9716d0
Sadashiv <sadamsrit@gmail.com>
2013-12-16 20:14:56 +0530

    Added the class

--- python/classes.py
+++ python/classes.py
@@ -1,20 +1,8 @@
-class MyClass:
-    """A simple example class"""
-    i = 12345
-    def f(self):
-        return 'hello world'
-
-x=MyClass()
-print x
-
-Inheritance
-class DerivedClassName(BaseClassName):
-    <statement-1>
-    <statement-N>
-
-Multiple Inheritance
-class DerivedClassName(Base1, Base2, Base3):
-    <statement-1>
-    <statement-n>
-
-Generators are a simple and powerful tool for creating iterators.
+class Point():
+    pass
+    print "hello"
+obj = Point()
+obj.x = 10
+obj.y = 20
+print obj.x,obj.y
+print obj
